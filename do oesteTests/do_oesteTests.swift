26b6667fb162790fe1b459deec8fac8644309665
Eduardo Lopes de Carvalho <eduardo.lopes.c@gmail.com>
2020-08-17 14:32:04 -0300

    Initial Commit

---
+++ do oesteTests/do_oesteTests.swift
@@ -0,0 +1,34 @@
+//
+//  do_oesteTests.swift
+//  do oesteTests
+//
+//  Created by Eduardo Lopes de Carvalho on 17/08/20.
+//  Copyright © 2020 Eduardo Lopes de Carvalho. All rights reserved.
+//
+
+import XCTest
+@testable import do_oeste
+
+class do_oesteTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
